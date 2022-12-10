dd7bdc7fa649536ceb956f8b048d11ab4d386889
Cassim Y Patel <cassim.y.patel@pwc.com>
2022-12-10 15:05:13 +0000

    Added empty solutions for day 8

---
+++ AoC_2022/day08/solutions.py
@@ -0,0 +1,15 @@
+def day8_part1(input):
+    return None
+
+def day8_part2(input):
+    return None
+
+if __name__ == "__main__":
+    example_input = open('example.txt', 'r').read()
+    test_input = open('input.txt', 'r').read()
+
+    assert day8_part1(example_input) == 21
+    print(day8_part1(test_input))
+
+    # assert day8_part2(example_input) == 24933642
+    # print(day8_part2(test_input))
