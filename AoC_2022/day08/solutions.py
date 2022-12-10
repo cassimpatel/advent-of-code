a04ae85eaa153ab7b2ea8a8e22982eb1f1aa0b55
Cassim Y Patel <cassim.y.patel@pwc.com>
2022-12-10 15:55:50 +0000

    Completed day 8 part 1

--- AoC_2022/day08/solutions.py
+++ AoC_2022/day08/solutions.py
@@ -1,5 +1,37 @@
 def day8_part1(input):
-    return None
+    trees = [[int(y) for y in x] for x in input.split('\n')]
+    gridW = len(trees[0])
+    gridH = len(trees)
+
+    dir_vect = {'U':[0, 1], 'D':[0, -1], 'L':[-1, 0], 'R':[1, 0]}
+    numVisibile = (gridH * 2) + (gridW - 2) * 2
+
+    # for each tree in the forest
+    for y in range(1, gridH - 1):
+        for x in range(1,  gridW - 1):
+
+            # print(y, x, trees[y][x])
+            visible = False
+
+            # for each dir, traverse in that direction
+            # if reach and edge mark as visible, otherwise break if reach taller tree
+            for dir in dir_vect:
+                [dx, dy] = dir_vect[dir]
+                mult = 1
+
+                while not visible:
+                    newX, newY = x + mult * dx, y + mult * dy
+                    # print('curr', newY, newX)
+                    if newX in [-1, gridW] or newY in [-1, gridH]:
+                        visible = True
+                    elif trees[newY][newX] >= trees[y][x]:
+                        break
+                    mult += 1
+                
+                if visible: break
+            if visible: numVisibile += 1
+
+    return numVisibile
 
 def day8_part2(input):
     return None
