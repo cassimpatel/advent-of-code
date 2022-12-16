2b014b623b8106f589f4a456303cd06a610fb5d5
Cassim Y Patel <cassim.y.patel@pwc.com>
2022-12-16 08:30:05 +0000

    Corrected function naming for day

--- AoC_2022/day15/solutions.py
+++ AoC_2022/day15/solutions.py
@@ -8,7 +8,7 @@ def getSensorData(input):
         sensors[i].append(manhattanDist)
     return sensors
 
-def day14_part1(input, y):
+def day15_part1(input, y):
     sensors = getSensorData(input)
 
     covered = set([])
@@ -39,7 +39,7 @@ def generatePoints(x, y, d):
         points.update(newPoints)
     return points
 
-def day14_part2(input, maxXY):
+def day15_part2(input, maxXY):
     sensors = getSensorData(input)
 
     for [sx, sy, bx, by, d] in sensors:
@@ -66,8 +66,8 @@ if __name__ == "__main__":
     example_input = open('example.txt', 'r').read()
     test_input = open('input.txt', 'r').read()
 
-    assert day14_part1(example_input, 10) == 26
-    print(day14_part1(test_input, 2000000))
+    assert day15_part1(example_input, 10) == 26
+    print(day15_part1(test_input, 2000000))
 
-    assert day14_part2(example_input, 20) == 56000011
-    print(day14_part2(test_input, 4000000))
+    assert day15_part2(example_input, 20) == 56000011
+    print(day15_part2(test_input, 4000000))
