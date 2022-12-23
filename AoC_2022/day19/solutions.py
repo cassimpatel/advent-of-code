091ab82fa4c992215cb721f5429c609ff8e9e298
Cassim Y Patel <cassim.y.patel@pwc.com>
2022-12-23 17:43:54 +0000

    Added template solution files for remaining days

---
+++ AoC_2022/day19/solutions.py
@@ -0,0 +1,15 @@
+def day19_part1(input):
+    return None
+
+def day19_part2(input):
+    return None
+
+if __name__ == "__main__":
+    example_input = open('example.txt', 'r').read()
+    test_input = open('input.txt', 'r').read()
+
+    assert day19_part1(example_input) == 33
+    print(day19_part1(test_input))
+
+    # assert day19_part2(example_input) == 1
+    # print(day19_part2(test_input))
---
+++ AoC_2022/day20/solutions.py
@@ -0,0 +1,15 @@
+def day20_part1(input):
+    return None
+
+def day20_part2(input):
+    return None
+
+if __name__ == "__main__":
+    example_input = open('example.txt', 'r').read()
+    test_input = open('input.txt', 'r').read()
+
+    assert day20_part1(example_input) == 3
+    print(day20_part1(test_input))
+
+    # assert day20_part2(example_input) == 1
+    # print(day20_part2(test_input))
