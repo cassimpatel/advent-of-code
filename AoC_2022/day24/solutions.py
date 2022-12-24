5f6068caaf15bd376301211b124b9bf3c063dd3c
Cassim Y Patel <cassim.y.patel@pwc.com>
2022-12-24 13:42:17 +0000

    Added example and input for day 24

--- AoC_2022/day24/solutions.py
+++ AoC_2022/day24/solutions.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     example_input = open('example.txt', 'r').read()
     test_input = open('input.txt', 'r').read()
 
-    assert day24_part1(example_input) == 1
+    assert day24_part1(example_input) == 18
     print(day24_part1(test_input))
 
     # assert day24_part2(example_input) == 1
