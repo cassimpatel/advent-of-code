d513abc76633dd955af117bcf2981a7ad26defba
Cassim Y Patel <cassim.y.patel@pwc.com>
2022-12-24 21:44:01 +0000

    Completed day 16 part 1

--- AoC_2022/day16/solutions.py
+++ AoC_2022/day16/solutions.py
@@ -1,4 +1,5 @@
 import re
+import math
 from queue import PriorityQueue
 
 def parseInput(input):
@@ -11,45 +12,58 @@ def parseInput(input):
         tunnels[valve] = [x.replace(',', '') for x in splitLine[9:]]
     return flow, tunnels
 
+# finds the shortest path length between every two valves using Floyd Warshall
+def shortestPaths(tunnels):
+    # initialise paths lengths as 1 if connected, 0 to self, infinity otherwise
+    paths = {(a,b):0 if a == b else 1 if b in tunnels[a] else math.inf for a in tunnels for b in tunnels}
+
+    for m in tunnels:
+        new_paths = {}
+        for i in tunnels:
+            for j in tunnels:
+                new_paths[(i, j)] = min(paths[(i, j)], paths[(i, m)] + paths[(m, j)])
+        paths = new_paths
+
+    return paths
+
 def day16_part1(input):
     flow, tunnels = parseInput(input)
+    dists = shortestPaths(tunnels)
 
-    # we will characterise a configuration as time left, current loc, open valves, current flow
-    initialConfig = (31, 'AA', set([]), 0)
-    frontier = PriorityQueue()
-    frontier.put((0, initialConfig))
-    # frontier = [initialConfig]
+    # filter down distances to if they connect two >0 caves or start from AA
+    dists = {(a, b):v for ((a, b), v) in dists.items() if (flow[a]*flow[b]>0) or (a=='AA' and flow[b]>0)}
+    flow = {x: flow[x] for x in flow if flow[x] > 0}
 
+    # we will characterise a configuration as time left, current loc, open valves, current flow since start
+    initialConfig = (30, 'AA', set([]), 0)
+    frontier = [initialConfig]
     maxFinalFlow = 0
     
-    while not frontier.empty():
-        (priority, (timeLeft, loc, openV, currFlow)) = frontier.get()
-        print(timeLeft, loc, openV, currFlow)
+    while len(frontier) > 0:
+        (timeLeft, loc, openV, pastFlow) = frontier.pop(0)
+        # print(timeLeft, loc, openV, pastFlow)
 
-        if timeLeft == 0:
-            print(currFlow)
-            return
-            maxFinalFlow = max(currFlow, maxFinalFlow)
+        currFlow = sum([flow[x] for x in openV])
+        nextCaves = [x for x in flow if x not in openV and timeLeft >= dists[(loc, x)] + 1]
+
+        if len(nextCaves) == 0:
+            # all >0 valves visited or not enough time left to visit more, calculate flow generated during remaining time
+            newFlow = pastFlow + timeLeft * currFlow
+            maxFinalFlow = max(newFlow, maxFinalFlow)
             continue
 
-        nextConfigs = []
-
-        newFlow = currFlow + sum([flow[x] for x in openV])
-        priority = - newFlow
-        if flow[loc] > 0 and loc not in openV:
-            # nextConfigs.append((timeLeft-1, loc, openV.union([loc]), newFlow))
-            frontier.put((priority-flow[loc], (timeLeft-1, loc, openV.union([loc]), newFlow)))
-        for neighbour in tunnels[loc]:
-            # nextConfigs.append((timeLeft-1, neighbour, openV, newFlow))
-            potentialFlow = 0 if neighbour in openV else flow[neighbour]
-            frontier.put((priority-potentialFlow, (timeLeft-1, neighbour, openV, newFlow)))
-        
-        # frontier += nextConfigs
-
+        for nxt in nextCaves:
+            newTimeLeft = timeLeft - dists[(loc, nxt)] - 1
+            newFlow = pastFlow + currFlow * (dists[(loc, nxt)] + 1)
+            frontier.insert(0, (newTimeLeft, nxt, openV.union([nxt]), newFlow))
 
     return maxFinalFlow
 
 def day16_part2(input):
+    # approach: similar to part 1 but include the elephants loc as another parameter
+    # additionally decrease time left on start state to 26
+    # elephant and self can reach their destinations in different times so track time remaining for both to get to their next goal in the state (whichever comes first, remove same time from the max one too)
+    # when considering which cave to go to next, try all choices for both
     return None
 
 if __name__ == "__main__":
@@ -59,5 +73,5 @@ if __name__ == "__main__":
     assert day16_part1(example_input) == 1651
     print(day16_part1(test_input))
 
-    # assert day16_part2(example_input) == 56000011
+    assert day16_part2(example_input) == 1707
     # print(day16_part2(test_input)
