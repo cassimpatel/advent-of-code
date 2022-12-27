f313ac5e8c5bbe0a5ea9f29e63e78bbc8eee5194
Cassim Y Patel <cassim.y.patel@pwc.com>
2022-12-27 21:44:30 +0000

    Added notes for day 19

--- AoC_2022/day19/solutions.py
+++ AoC_2022/day19/solutions.py
@@ -1,4 +1,12 @@
 def day19_part1(input):
+    # separate out a function to calculate the score for a single blueprint so we can iterate over blueprints easily
+    # this function will do a simple DFS to maximise the score, with agressive pruning where possible
+    # we should never have more robots of any given type than the maximum required of that resource for any robot creation (as we can only make one robot a round and resources replenish)
+    # if we can make a geode robot that is the optimal move: we shouldn't explore other options
+    # taking the current best score, if we can't do better than it in the remaining time (assume making a geode robot every remaining turn) that node has no potential, pune it
+    # make sure to keep a dict of where nodes have been visited from so you can backtrack from the optimal solution
+    # if you can make a robot, only consider waiting if waiting means you could build a different robot in the future (b has cost strictly more than a)
+    # represent states as (timeRemaining, ore, oreR, cla, claR, obs, obsR, geo, geoR)
     return None
 
 def day19_part2(input):
