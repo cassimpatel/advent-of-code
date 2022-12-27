e6e6729995ce3f0cf0fd3f382e3c74721d0bd53d
Cassim Y Patel <cassim.y.patel@pwc.com>
2022-12-27 14:22:45 +0000

    Completed day 17 pt 1

--- AoC_2022/day17/solutions.py
+++ AoC_2022/day17/solutions.py
@@ -1,15 +1,54 @@
+# returns new rock location and whether or not it has moved
+def move_rock(chamber, rock, dir):
+    dirs = {'<':(-1, 0), '>':(1, 0), 'v':(0, -1)}
+    dx, dy = dirs[dir]
+
+    x = [x for (x, y) in rock]
+    minX, maxX = min(x), max(x)
+
+    newRock = []
+
+    if dir == '<' and minX == 0: return rock, False
+    if dir == '>' and maxX == 6: return rock, False
+
+    for (x, y) in rock:
+        nx, ny = x + dx, y + dy
+        if (nx, ny) in chamber: return rock, False
+        newRock.append((nx, ny))
+
+    return newRock, True
+    
 def day17_part1(input):
-    # 1 means right and 0 means left
-    jets = [-1 if x == '<' else 1 for x in input]
-    tower = [0] * 7
-
-    # rocks data encoded as heights of the bottom of the boulder then heights of the top of the boulder
-    # this moves across the 4 width to account for the widest 
+    jets = [x for x in input]
     rocks = [
-
+        [(2, 0), (3, 0), (4, 0), (5, 0)],
+        [(2, 1), (3, 0), (3, 1), (3, 2), (4, 1)],
+        [(2, 0), (3, 0), (4, 0), (4, 1), (4, 2)],
+        [(2, 0), (2, 1), (2, 2), (2, 3)],
+        [(2, 0), (2, 1), (3, 0), (3, 1)]
     ]
+    chamber = set([(x, 0) for x in range(7)])
 
-    return None
+    jet = 0
+    maxHeight = 0
+
+    for i in range(2022):
+        currRock = [(x, maxHeight + 4 + y) for (x, y) in rocks[i % len(rocks)]]
+        settled = False
+
+        while not settled:
+            currJet = jets[jet]
+            
+            currRock, _ = move_rock(chamber, currRock, currJet)
+            currRock, moved = move_rock(chamber, currRock, 'v')
+
+            if not moved:
+                settled = True
+                chamber.update(currRock)
+                maxHeight = max(maxHeight, max([y for (x, y) in currRock]))
+
+            jet = (jet + 1) % len(jets)
+    return maxHeight
 
 def day17_part2(input):
     return None
@@ -18,8 +57,8 @@ if __name__ == "__main__":
     example_input = open('example.txt', 'r').read()
     test_input = open('input.txt', 'r').read()
 
-    assert day17_part1(example_input) == 13068
+    assert day17_part1(example_input) == 3068
     print(day17_part1(test_input))
 
-    # assert day17_part2(example_input) == 56000011
+    # assert day17_part2(example_input) == 1514285714288
     # print(day17_part2(test_input)
