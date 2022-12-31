733a2a260886773b165623e55f10abd23b7eee2d
Cassim Y Patel <cassim.y.patel@pwc.com>
2022-12-31 13:52:54 +0000

    Completed day 22 part 2

--- AoC_2022/day22/solutions.py
+++ AoC_2022/day22/solutions.py
@@ -5,8 +5,8 @@ def parse_input(input):
     grid = [[y for y in x] for x in input.split('\n\n')[0].split('\n')]
 
     # padding grid with ' ' to allow for easier boundary checking later
-    maxW = max([len(x) for x in grid])
-    grid = [[' '] * (maxW + 2)] + [[' '] + x + [' '] * (maxW - len(x) + 1) for x in grid] + [[' '] * (maxW + 2)]
+    maxW = max([len(x) for x in grid]) + 2
+    grid = [[' '] * maxW] + [[' '] + x + [' '] * (maxW - len(x) - 1) for x in grid] + [[' '] * maxW]
 
     return grid, cmds
 
@@ -26,63 +26,45 @@ def basic_wrap_around(grid, x, y, dirIndex):
 # wraps around map according to rules in part 2 (the map is a cube)
 # hardcodes due to all test inputs having the same cube layout (doesn't work on example input)
 def cube_wrap_around(grid, x, y, dirIndex):
-    dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)] # > v < ^
-    nx, ny, newDirIndex = 0, 0, 0
-
     # map down by one in each direction to account for padding added
-    x, y = x-1, y-1
+    x, y = x - 1, y - 1
     # calculate face in each direction and offset from its face
-    faceX, faceY = x // 50, y // 50
-    offsetX, offsetY = x % 50, y % 50
+    faceX, offsetX = x // 50, x % 50
+    faceY, offsetY = y // 50, y % 50
 
-    # TODO: a dictionary mapping (faceX, faceY, dirIndex) -> (nx, ny, newDirIndex) to avoid using lengthy ifs
-
-    # looking at each faces 1...6 in https://imgur.com/a/K9Of42d for reference
-    # for each of these faces, considering directions from this face to the void
-    if (faceX, faceY) == (2, 0):
+    # maps (faceX, faceY, dirIndex) -> (nx, ny, newDirIndex) according to https://imgur.com/a/K9Of42d
+    # we only consider the ways you can enter the void from the map
+    cube_wrapping = {
         # face 1: can go >, v or ^
-        if dirIndex == 0:
-            nx, ny, newDirIndex = 99, 99 + (50 - offsetY), 2
-        elif dirIndex == 1:
-            nx, ny, newDirIndex = 99, 50 + (offsetX), 2
-        elif dirIndex == 3:
-            nx, ny, newDirIndex = offsetX, 199, 3
-    elif (faceX, faceY) == (1, 0):
-        # face 2: can go < or ^
-        if dirIndex == 2:
-            nx, ny, newDirIndex = 0, 99 + (50 - offsetY), 0
-        elif dirIndex == 3:
-            nx, ny, newDirIndex = 0, 150 + offsetX, 0
-    elif (faceX, faceY) == (1, 1):
-        # face 3: can go > or <
-        if dirIndex == 0:
-            nx, ny, newDirIndex = 100 + offsetY, 49, 3
-        elif dirIndex == 2:
-            nx, ny, newDirIndex = offsetY, 100, 1
-    elif (faceX, faceY) == (1, 2):
-        # face 4: can go > or v
-        if dirIndex == 0:
-            nx, ny, newDirIndex = 0, 0, 0
-        elif dirIndex == 1:
-            nx, ny, newDirIndex = 0, 0, 0
-    elif (faceX, faceY) == (0, 2):
-        # face 5: can go < or ^
-        if dirIndex == 2:
-            nx, ny, newDirIndex = 0, 0, 0
-        elif dirIndex == 3:
-            nx, ny, newDirIndex = 0, 0, 0
-    elif (faceX, faceY) == (0, 3):
-        # face 6: can go >, v or <
-        if dirIndex == 0:
-            nx, ny, newDirIndex = 0, 0, 0
-        elif dirIndex == 1:
-            nx, ny, newDirIndex = 0, 0, 0
-        elif dirIndex == 2:
-            nx, ny, newDirIndex = 0, 0, 0
+        (2, 0, 0): (99,      99 + (50 - offsetY), 2),
+        (2, 0, 1): (99,      50 + (offsetX),      2),
+        (2, 0, 3): (offsetX, 199,                 3),
 
-    # mapping back to grid indices with padding
-    nx, ny = nx + 1, ny + 1
-    return nx, ny, newDirIndex
+        # face 2: can go < or ^
+        (1, 0, 2): (0, 99 + (50 - offsetY), 0),
+        (1, 0, 3): (0, 150 + offsetX,       0),
+
+        # face 3: can go > or <
+        (1, 1, 0): (100 + offsetY, 49,  3),
+        (1, 1, 2): (offsetY,       100, 1),
+
+        # face 4: can go > or v
+        (1, 2, 0): (149, 49 - offsetY,  2),
+        (1, 2, 1): (49,  150 + offsetX, 2),
+
+        # face 5: can go < or ^
+        (0, 2, 2): (50, 49 - offsetY, 0),
+        (0, 2, 3): (50, 50 + offsetX, 0),
+
+        # face 6: can go >, v or <
+        (0, 3, 0): (50 + offsetY,  149, 3),
+        (0, 3, 1): (100 + offsetX, 0,   1),
+        (0, 3, 2): (50 + offsetY,  0,   1)
+    }
+
+    nx, ny, newDirIndex = cube_wrapping[(faceX, faceY, dirIndex)]
+    # map back to grid indices with padding
+    return nx + 1, ny + 1, newDirIndex
 
 # takes raw input and function to handle map wrap around
 def get_password(input, wrap_around_func):
@@ -99,6 +81,7 @@ def get_password(input, wrap_around_func):
                 nx, ny = x + dx, y + dy
                 
                 if grid[ny][nx] == ' ':
+                    # proposed location is in void: find new loc, dir by wrap around func
                     nx, ny, dirIndex = wrap_around_func(grid, x, y, dirIndex)
                 
                 if grid[ny][nx] == '#': break
@@ -106,6 +89,7 @@ def get_password(input, wrap_around_func):
         else:
             dirIndex = (dirIndex + 1) % 4 if cmd == 'R' else (dirIndex - 1) % 4
         # print("command: {}, loc: {}, dir: {}".format(cmd, (x-1, y-1), dirs[dir]))
+    
     return 1000 * y + 4 * x + dirIndex
 
 def day22_part1(input):
@@ -113,11 +97,6 @@ def day22_part1(input):
 
 def day22_part2(input):
     return get_password(input, cube_wrap_around)
-    # same to previous solution but when running off a side: send x, y and dx, dy to a function
-    # function will look at % 50 (or 4 if test input) to determine which face being walked off of
-    # can then map back to exact location using offset from original face
-    
-    return None
 
 if __name__ == "__main__":
     example_input = open('example.txt', 'r').read()
@@ -126,5 +105,6 @@ if __name__ == "__main__":
     assert day22_part1(example_input) == 6032
     print(day22_part1(test_input))
 
+    # solution is hardcoded to test input size and format: don't run for example
     # assert day22_part2(example_input) == 1
-    # print(day22_part2(test_input))
+    print(day22_part2(test_input))
